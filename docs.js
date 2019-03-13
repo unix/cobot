1810364dda5c99105d73971b3d4c1dc84f8613bf
WittBulter <nanazuimeng123@gmail.com>
2019-03-13 14:55:13 +0800

    build: add favicon

--- docs.js
+++ docs.js
@@ -3,8 +3,8 @@ const { execSync } = require('child_process')
 const es = fs.readFileSync('./README.md', 'utf-8')
 
 const init = t => `<head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Cobot</title>
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@zeit-ui/style@latest/dist/style.css"</head><body class="hack"><div style="max-width: 1000px;margin: 0 auto;padding: 30px 0;">
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@zeit-ui/style@latest/dist/style.css"><link rel="icon" href="favicon.ico"></head><body class="hack"><div style="max-width: 1000px;margin: 0 auto;padding: 30px 0;">
 ${require('marked')(t)}<hr>This project is open-sourced on <a href="https://github.com/WittBulter/cobot">GitHub</a>.</div></body>`
 
-execSync('(yarn add marked & mkdir dist) && cp -R .github ./dist')
+execSync('(yarn add marked & mkdir dist) && cp -R .github ./dist && cp favicon.ico ./dist')
 fs.writeFileSync('dist/index.html', init(es))
