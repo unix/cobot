5310d6bce4e7fa912af0e88c3aea3b925debf082
WittBulter <nanazuimeng123@gmail.com>
2019-03-22 11:57:29 +0800

    0.1.2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Caller } from './caller'
 import { CustomEmitter, Settings } from './utils/shared'
 import { GITLAB } from './contants'
-const BotEvents = GITLAB.BotEvents
+const { BotEvents, HEADERS } = GITLAB
 export {
   BotEvents,
+  HEADERS,
 }
+
+export type BotEventContext = GITLAB.EventContext
 
 export default new class Cobot extends Caller {
   lift(settings?: Settings): CustomEmitter {
